38e18565c89c3c93a672fe1d5872aeceb7ddf4ec
Yago González <yagogonzalezg@gmail.com>
2018-07-30 12:27:27 +0200

    Strip extraneous parameters from requests
    Any parameter that isn't explicitly validated will be removed from the
    request object received by the controller. This helps keep things more
    controlled, and also allows to safely use the requests' parameters to
    construct or edit documents in the DB.

--- controllers/validators.js
+++ controllers/validators.js
@@ -1,5 +1,11 @@
 const Joi = require('joi');
 
+// Remove any parameter that isn't explicitly validated here. Useful for using
+// default values in Mongoose models as automatic setters
+Joi.any().options({
+    stripUnknown: true,
+});
+
 module.exports = {
     login: {
         body: {
