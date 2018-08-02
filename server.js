58e12b5746eac26259773561987a09988a08b4b9
charlascuen <carlos.cuencae@alumnos.upm.es>
2018-08-02 08:43:18 +0200

    Add JSON body parser middleware

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const { globalErrorHandler } = require('./common/errors');
 require('./models/UserModel');
 
 // Parser for the requests' body
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 // Session storage
 app.use(session({
