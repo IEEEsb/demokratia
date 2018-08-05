a11781e29ed39fc7dbab3649ed7d7c45d03d33fd
Yago González <yagogonzalezg@gmail.com>
2018-08-05 21:40:30 +0200

    Adapt Poll's fields to the Election-Poll structure
    The "Poll" schema has been modified to hold the required information,
    now that a "Poll" is the model under "Elections" in the data hierarchy.

--- models/Poll.js
+++ models/Poll.js
@@ -3,10 +3,18 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const Poll = new Schema({
+	// An identifier for the poll, like the charge being chosen
 	name: { type: String, required: true, unique: true },
-	title: { type: String, required: true },
-	shortDescription: { type: String },
-	longDescription: { type: String },
-});
+	// A human-readable question that makes clear what is being voted in the
+	// poll
+	question: { type: String, required: true },
+	// A longer description of the poll, like details on what the specific
+	// charge should do
+	description: { type: String },
+	// The possible non-blank votes for this poll
+	candidates: [{ type: Schema.Types.ObjectId, unique: true }],
+	// Don't create IDs for polls in order to keep them unique when adding them
+	// to an election
+}, { _id: false });
 
 module.exports = mongoose.model('Poll', Poll);
