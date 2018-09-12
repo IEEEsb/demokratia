79411dfb3cc78149f5756261609ddbc874ad5995
charlascuen <carlos.cuencae@alumnos.upm.es>
2018-09-12 12:14:48 +0200

    minor fixes

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,7 +37,7 @@ module.exports.login = (req, res, next) => {
 			// Check that the password is correct
 			if (!isValid) throw new CredentialsError();
 			if (!user.roles.includes(votingRole)
-				|| !user.roles.includes(adminRole)) throw new MissingRolesError();
+				&& !user.roles.includes(adminRole)) throw new MissingRolesError();
 
 			// Successful login. Set the session up and send the minimal set of
 			// user information required by the application
